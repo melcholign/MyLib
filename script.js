66ab651f6739ac7104a0cb62deba4e715f82854b
melcholign <115886450+melcholign@users.noreply.github.com>
2024-06-14 15:26:07 +0600

    Represent library and book in code

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBookToLibrary() {
+    // get user input to create a new Book object
+    const book = undefined;
+    myLibrary.push(book);
+}
